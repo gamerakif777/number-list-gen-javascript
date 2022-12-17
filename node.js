4d752ec43f93929fa0f6787538a884b1fc3e4772
Akif <109610453+gamerakif777@users.noreply.github.com>
2022-12-17 12:15:41 -0500

    Add files via upload

---
+++ node.js
@@ -0,0 +1,12 @@
+console.log("Enter a starting number, ending number and a set increment");
+console.log();
+
+var x = parseInt(prompt("Starting Value: "))
+
+var y = parseInt(prompt("Ending Value: "))
+
+var z = parseInt(prompt("Increment: "))
+
+for (let number = 0; i < x,y,z; number++){
+  console.log(Number);
+}
